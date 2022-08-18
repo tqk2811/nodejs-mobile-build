bad9e45681ce209c1626ebc476272f949fb05c45
tqk2811 <tqk2811@gmail.com>
2022-08-18 17:46:01 +0700

    update build execute & build32

--- build-node-execute/build32.sh
+++ build-node-execute/build32.sh
@@ -2,7 +2,7 @@
 export NDK=../android-ndk-r25
 export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
 export PATH=$PATH:$TOOLCHAIN/bin
-export TARGET=aarch64-linux-android
+export TARGET=armv7a-linux-androideabi
 export API=24
 export CC=$TOOLCHAIN/bin/$TARGET$API-clang
 export AS=$CC
@@ -11,16 +11,16 @@ export AR=$TOOLCHAIN/bin/llvm-ar
 export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
 export STRIP=$TOOLCHAIN/bin/llvm-strip
 export SYSROOT=$TOOLCHAIN/sysroot
-export NDK_LINK=$SYSROOT/usr/lib/$TARGET
+export NDK_LINK=$SYSROOT/usr/lib/arm-linux-androideabi
 
 #Nodejs
-export NODE=../out/aarch64/usr/local
+export NODE=../out/armv7a/usr/local
 export LNODE=$NODE/lib
 export INODE=$NODE/include/node
 export PATH=$PATH:$LNODE
 
 #output
-export OUT=out/aarch64
+export OUT=out/armv7a
 # CFLAGS_BASE_LIST="-c -D_7ZIP_AFFINITY_DISABLE=1 -pipe -fPIE -fexceptions -frtti \
 #         -mcpu=cortex-a53+crc+crypto -Wno-suggest-override -Wno-suggest-destructor-override \
 #         -ffunction-sections -fdata-sections -Wl,--gc-sections" \
